f0794bb4aec06d3ef922b2848e2adad389a2ee0c
RegianeJulia <81393045+RegianeJulia@users.noreply.github.com>
2024-06-10 11:11:04 +0930

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,37 @@
+console.log(document.getElementById("btn-avancar"));
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar")
+let cartaoAtual = 0
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function () {
+    const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
+    if (ehUltimoCartao) return;
+
+
+    escondercartaoselecionado();
+
+    cartaoAtual++;
+    mostrarcartao();
+
+});
+
+btnVoltar.addEventListener("click", function () {
+    const ehPrimeiroCartao = cartaoAtual === 0;
+    if (ehPrimeiroCartao) return;
+
+    escondercartaoselecionado();
+
+    cartaoAtual--;
+    mostrarcartao();
+
+});
+
+function mostrarcartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function escondercartaoselecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
